c66bb536ec16f0d368bac81e2aa8e51a9f6195bf
markus_go <markus.gottschalk@student.htw-berlin.de>
2016-10-23 23:45:15 +0200

    Added condition "hungry" and "general satisfaction"

--- Assets/Source/Script/AI/StupidIntelligence.cs
+++ Assets/Source/Script/AI/StupidIntelligence.cs
@@ -47,9 +47,11 @@ public class StupidIntelligence : Intelligence {
             }
         }
     }
+    
 
     //Statuses
     private Dictionary<string, int> receivedBall = new Dictionary<string, int> { {"Creature eats the ball...", -5}, {"Your pet is playing with it.", 0}, {"The ball gets destroyed.", 3} };
+    private Dictionary<string, int> conditions = new Dictionary<string, int> { {"hungry", 0}, { "general satisfaction", 80 } };
 
     //Feedback
     private Dictionary<string, int> lastActionSet;
@@ -96,34 +98,75 @@ public class StupidIntelligence : Intelligence {
 			{
 			case -1:
 				UIManager.Instance.ReceiveMessage ("It seems to feel bad about it.");
+                conditions["general satisfaction"] -= 3;
                 actionsReward -= 3;
 				_waitForAnswer = false;
 				break;
 			case 0:
 				UIManager.Instance.ReceiveMessage ("It stares blanky at you.");
-				_waitForAnswer = false;
+                conditions["general satisfaction"] -= 1;
+                _waitForAnswer = false;
                 actionsReward -= 1;
                 break;
 			case 1:
 				UIManager.Instance.ReceiveMessage ("It claps its hands in glee.");
+                conditions["general satisfaction"] += 3;
 				_waitForAnswer = false;
                 actionsReward += 3;
                 break;
 			}
 
             lastActionSet[lastAction] = actionsReward;
+
+            foreach(KeyValuePair<string, int> state in conditions)
+            {
+                Debug.Log("condition: " + state.Key + ": " + state.Value);
+            }
 		}
 	}
 
     public override void receiveBall()
     {
+
+        if(conditions["hungry"] > 20)
+        {
+            UIManager.Instance.ReceiveMessage("It is hungry and wants to eat something...");
+            List<string> keys = new List<string>(receivedBall.Keys);
+            foreach(string entry in keys)
+            {
+                if (entry.Contains("eat"))
+                {
+                    receivedBall[entry] += 10;
+                }
+            }
+        }
+
         foreach (KeyValuePair<string, int> entry in receivedBall)
         {
             Debug.Log("Action: " + entry.Key + " gives " + entry.Value + " reward points.");
         }
-        lastActionSet = receivedBall;
-        lastAction = receivedBall.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
-        UIManager.Instance.ReceiveMessage(lastAction);
-        _waitForAnswer = true;
+
+
+        if (conditions["general satisfaction"] < 30)
+        {
+            UIManager.Instance.ReceiveMessage("It doesn't want to play anymore...");
+
+            conditions["general satisfaction"]++;
+        }
+        else
+        {
+            lastActionSet = receivedBall;
+            lastAction = receivedBall.Aggregate((l, r) => l.Value > r.Value ? l : r).Key;
+            UIManager.Instance.ReceiveMessage(lastAction);
+            _waitForAnswer = true;
+        }
+
+
+        conditions["hungry"] += 1;
+        if (lastAction.Contains("eat"))
+        {
+            conditions["hungry"] -= 20;
+        }
+
     }
 }
