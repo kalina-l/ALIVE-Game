8548eecb9fbf654ad2ec0d46f9d9349a6d29d6ca
SERENA\Kalina <kalina.lebecka@gmail.com>
2016-11-07 11:52:58 +0100

    Ball and Food can now be added to a scene as items

--- Assets/Source/Script/Scene/SceneManager.cs
+++ Assets/Source/Script/Scene/SceneManager.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 
 public class SceneManager : Singleton<SceneManager> {
 
+    public GameObject ballPrefab;
+    public GameObject foodPrefab;
+
+    public GameObject itemsGroup;
+
     private Ball ball;
     private Food food;
 
-    public void addBall(Ball ball) {
-        this.ball = ball;
+    public void addBall() {
+        if (this.ball == null) {
+            GameObject ball = (GameObject)Instantiate(ballPrefab);
+            ball.transform.parent = itemsGroup.transform;
+            this.ball = ball.GetComponent<Ball>();
+        }
     }
 
-    public void addFood(Food food) {
-        this.food = food;
+    public void takeBall() {
+        if (ball != null) {
+            Destroy(ball.gameObject);
+        }
+        ball = null;
+    }
+
+    public void addFood() {
+        if (this.food == null) {
+            GameObject food = (GameObject)Instantiate(foodPrefab);
+            food.transform.parent = itemsGroup.transform;
+            this.food = food.GetComponent<Food>();
+        }
+    }
+
+    public void takeFood() {
+        if (food != null) {
+            Destroy(food.gameObject);
+        }
+        food = null;
     }
 
     public List<Item> getItems() {
--- Assets/Source/Script/UI/UIManager.cs
+++ Assets/Source/Script/UI/UIManager.cs
@@ -5,61 +5,68 @@ using System.Collections;
 
 public class UIManager : MonoBehaviour {
 
-	public Button ApproveButton;
-	public Button NeutralButton;
-	public Button DisapproveButton;
+    public Button ApproveButton;
+    public Button NeutralButton;
+    public Button DisapproveButton;
 
-	public Text OutputText;
+    public Text OutputText;
 
-	private int messages;
+    private int messages;
 
-	public static UIManager Instance;
+    public static UIManager Instance;
 
-	public Intelligence AI;
+    public Intelligence AI;
 
-	void Awake()
-	{
-		Instance = this;
-	}
+    void Awake() {
+        Instance = this;
+    }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
 
-		ApproveButton.onClick.AddListener (delegate {
-			SendFeedback(1);
-		});
+        ApproveButton.onClick.AddListener(delegate {
+            SendFeedback(1);
+        });
 
-		NeutralButton.onClick.AddListener (delegate {
-			SendFeedback(0);
-		});
+        NeutralButton.onClick.AddListener(delegate {
+            SendFeedback(0);
+        });
 
-		DisapproveButton.onClick.AddListener (delegate {
-			SendFeedback(-1);
-		});
-	
-	}
+        DisapproveButton.onClick.AddListener(delegate {
+            SendFeedback(-1);
+        });
 
-	public void SendFeedback(int score)
-	{
+    }
+
+    public void SendFeedback(int score) {
         //Debug.Log("Feedback: " + score);
-		AI.ReceiveFeedback (score);
-	}
+        AI.ReceiveFeedback(score);
+    }
 
-	public void ReceiveMessage(string msg)
-	{
-		OutputText.text += msg + "\n" + "\n";
+    public void ReceiveMessage(string msg) {
+        OutputText.text += msg + "\n" + "\n";
 
-		if(messages > 4)
-		{
-			OutputText.rectTransform.anchoredPosition += Vector2.up * 90;
-			OutputText.rectTransform.sizeDelta += Vector2.up * 90;
-		}
+        if (messages > 4) {
+            OutputText.rectTransform.anchoredPosition += Vector2.up * 90;
+            OutputText.rectTransform.sizeDelta += Vector2.up * 90;
+        }
 
-		messages++;
-	}
+        messages++;
+    }
 
-    public void actionBall()
-    {
-        AI.receiveBall();
+    public void giveBall() {
+        SceneManager.Instance.addBall();
+    }
+
+    public void takeBall() {
+        SceneManager.Instance.takeBall();
+    }
+
+    public void giveFood() {
+        SceneManager.Instance.addFood();
+    }
+
+    public void takeFood() {
+        SceneManager.Instance.takeFood();
     }
 }
