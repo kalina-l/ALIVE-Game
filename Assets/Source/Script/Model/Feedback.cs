45612aa129dea711a7cd585d45bacbfbc473c109
frankbastian <frank.bastian87@googlemail.com>
2016-12-09 12:37:55 +0100

    added more feedback spread

--- Assets/Source/Script/Model/Feedback.cs
+++ Assets/Source/Script/Model/Feedback.cs
@@ -10,6 +10,8 @@ public class Feedback {
     private float[] SatisfactionFeedback;
     private float[] SocialFeedback;
 
+    private int feedbackCounter;
+
     public Feedback()
     {
         EnergyFeedback = new float[8];
@@ -26,6 +28,8 @@ public class Feedback {
             SatisfactionFeedback[i] = 0f;
             SocialFeedback[i] = 0f;
         }
+
+        feedbackCounter = 0;
     }
 
     public void AddFeedback(Dictionary<NeedType, Evaluation> Needs, int feedBack)
@@ -41,40 +45,62 @@ public class Feedback {
                 case NeedType.ENERGY:
                     EnergyFeedback[valueIndex] += feedBack;
                     if (valueIndex > 0)
-                        EnergyFeedback[valueIndex - 1] += feedBack * 0.3f;
+                        EnergyFeedback[valueIndex - 1] += feedBack * 0.6f;
                     if (valueIndex < 7)
-                        EnergyFeedback[valueIndex + 1] += feedBack * 0.3f;
+                        EnergyFeedback[valueIndex + 1] += feedBack * 0.6f;
+                    if (valueIndex > 1)
+                        EnergyFeedback[valueIndex - 2] += feedBack * 0.3f;
+                    if (valueIndex < 6)
+                        EnergyFeedback[valueIndex + 2] += feedBack * 0.3f;
                     break;
                 case NeedType.HEALTH:
                     HealthFeedback[valueIndex] += feedBack;
                     if (valueIndex > 0)
-                        HealthFeedback[valueIndex - 1] += feedBack * 0.3f;
+                        HealthFeedback[valueIndex - 1] += feedBack * 0.6f;
                     if (valueIndex < 7)
-                        HealthFeedback[valueIndex + 1] += feedBack * 0.3f;
+                        HealthFeedback[valueIndex + 1] += feedBack * 0.6f;
+                    if (valueIndex > 1)
+                        HealthFeedback[valueIndex - 2] += feedBack * 0.3f;
+                    if (valueIndex < 6)
+                        HealthFeedback[valueIndex + 2] += feedBack * 0.3f;
                     break;
                 case NeedType.HUNGER:
                     HungerFeedback[valueIndex] += feedBack;
                     if (valueIndex > 0)
-                        HungerFeedback[valueIndex - 1] += feedBack * 0.3f;
+                        HungerFeedback[valueIndex - 1] += feedBack * 0.6f;
                     if (valueIndex < 7)
-                        HungerFeedback[valueIndex + 1] += feedBack * 0.3f;
+                        HungerFeedback[valueIndex + 1] += feedBack * 0.6f;
+                    if (valueIndex > 1)
+                        HungerFeedback[valueIndex - 2] += feedBack * 0.3f;
+                    if (valueIndex < 6)
+                        HungerFeedback[valueIndex + 2] += feedBack * 0.3f;
                     break;
                 case NeedType.SATISFACTION:
                     SatisfactionFeedback[valueIndex] += feedBack;
                     if (valueIndex > 0)
-                        SatisfactionFeedback[valueIndex - 1] += feedBack * 0.3f;
+                        SatisfactionFeedback[valueIndex - 1] += feedBack * 0.6f;
                     if (valueIndex < 7)
-                        SatisfactionFeedback[valueIndex + 1] += feedBack * 0.3f;
+                        SatisfactionFeedback[valueIndex + 1] += feedBack * 0.6f;
+                    if (valueIndex > 1)
+                        SatisfactionFeedback[valueIndex - 2] += feedBack * 0.3f;
+                    if (valueIndex < 6)
+                        SatisfactionFeedback[valueIndex + 2] += feedBack * 0.3f;
                     break;
                 case NeedType.SOCIAL:
                     SocialFeedback[valueIndex] += feedBack;
                     if (valueIndex > 0)
-                        SocialFeedback[valueIndex - 1] += feedBack * 0.3f;
+                        SocialFeedback[valueIndex - 1] += feedBack * 0.6f;
                     if (valueIndex < 7)
-                        SocialFeedback[valueIndex + 1] += feedBack * 0.3f;
+                        SocialFeedback[valueIndex + 1] += feedBack * 0.6f;
+                    if (valueIndex > 1)
+                        SocialFeedback[valueIndex - 2] += feedBack * 0.3f;
+                    if (valueIndex < 6)
+                        SocialFeedback[valueIndex + 2] += feedBack * 0.3f;
                     break;
             }
         }
+
+        feedbackCounter++;
     }
 
     public float GetFeedback(Dictionary<NeedType, Evaluation> Needs)
